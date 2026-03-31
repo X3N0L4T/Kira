3bf7e85171cfc38e6920848a4a3f61d9483d7726
X3N0 <49507911+X3N0L4T@users.noreply.github.com>
2026-03-31 17:18:41 -0500

    Linked OpenCV 4.12.0

--- ErKit/Main.cpp
+++ ErKit/Main.cpp
@@ -1,12 +1,13 @@
 #include "MainForm.h"
+#include <opencv2/opencv.hpp>
 
 using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThread]
 int main(array<String^>^ args) {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew ErKit::MainForm());
-	return 0;
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew ErKit::MainForm());
+    return 0;
 }
